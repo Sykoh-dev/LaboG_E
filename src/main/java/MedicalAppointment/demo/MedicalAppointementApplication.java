2cf5acbc71792e8942d35bba2c6358dbbb84c16c
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-09-27 16:51:11 +0200

    Gedev : Test permission

--- src/main/java/MedicalAppointment/demo/MedicalAppointementApplication.java
+++ src/main/java/MedicalAppointment/demo/MedicalAppointementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedicalAppointementApplication {
-
+	// Accès pour Gedev : autorisé
 	public static void main(String[] args) {
 		SpringApplication.run(MedicalAppointementApplication.class, args);
 	}
