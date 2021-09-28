796562b3be98883407a4afdaf4a50252097f11c3
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-09-28 11:55:19 +0200

    PatientDTO

--- src/main/java/MedicalAppointment/demo/dto/PatientDTO.java
+++ src/main/java/MedicalAppointment/demo/dto/PatientDTO.java
@@ -1,4 +1,15 @@
 package MedicalAppointment.demo.dto;
 
+import javax.persistence.Column;
+import java.util.Date;
+
 public class PatientDTO {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String adress;
+    private String mail;
+    private Date bateOfBirth;
+
 }
