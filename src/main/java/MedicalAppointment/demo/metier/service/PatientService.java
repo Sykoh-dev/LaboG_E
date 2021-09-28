edd05fceb460a4e4cb732d71d2558706d2d7fc15
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-09-28 13:55:46 +0200

    Crud, Patient, appointment services interfaces

---
+++ src/main/java/MedicalAppointment/demo/metier/service/PatientService.java
@@ -0,0 +1,7 @@
+package MedicalAppointment.demo.metier.service;
+
+import MedicalAppointment.demo.dto.PatientDTO;
+import MedicalAppointment.demo.metier.service.CrudService;
+
+public interface PatientService extends CrudService<PatientDTO, Long> {
+}
