a4a43cb4ed7e310ee46a90793d3c00787f115733
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-09-29 14:34:03 +0200

    Presentation : Add patient menu, doctor menu & appointment menu

--- src/main/java/MedicalAppointment/demo/MedicalAppointementApplication.java
+++ src/main/java/MedicalAppointment/demo/MedicalAppointementApplication.java
@@ -1,13 +1,26 @@
 package MedicalAppointment.demo;
 
+import MedicalAppointment.demo.presentation.MenuAppointment;
+import MedicalAppointment.demo.presentation.MenuDoctor;
+import MedicalAppointment.demo.presentation.MenuPatient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MedicalAppointementApplication {
 	// Accès pour Gedev : autorisé
 	public static void main(String[] args) {
-		SpringApplication.run(MedicalAppointementApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(MedicalAppointementApplication.class, args);
+
+		MenuPatient menuPatient = ctx.getBean(MenuPatient.class);
+		menuPatient.start();
+
+		MenuDoctor menuDoctor = ctx.getBean(MenuDoctor.class);
+		menuDoctor.start();
+
+		MenuAppointment menuAppointment = ctx.getBean(MenuAppointment.class);
+		menuAppointment.start();
 	}
 
 }
