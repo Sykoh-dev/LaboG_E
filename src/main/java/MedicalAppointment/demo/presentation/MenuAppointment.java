78491c16c1e0a0dce2ae790df3919e4bed6e0dbd
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-09-29 14:13:37 +0200

    Appointment menu

--- src/main/java/MedicalAppointment/demo/presentation/MenuAppointment.java
+++ src/main/java/MedicalAppointment/demo/presentation/MenuAppointment.java
@@ -1,4 +1,97 @@
 package MedicalAppointment.demo.presentation;
 
+import MedicalAppointment.demo.dto.AppointmentDTO;
+import MedicalAppointment.demo.exception.ElementAlreadyPresentException;
+import MedicalAppointment.demo.exception.ElementNotFoundException;
+import MedicalAppointment.demo.metier.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.Scanner;
+
 public class MenuAppointment {
+    private final AppointmentService service;
+    private final Scanner sc;
+
+    public MenuAppointment(AppointmentService service, @Qualifier("sc2") Scanner sc) {
+        this.service = service;
+        this.sc = sc;
+    }
+
+    public void start(){
+        int choix = 0;
+        do{
+            displayMenu();
+            choix = Integer.parseInt(sc.nextLine());
+            mapChoix(choix);
+        }while (choix != 6);
+    }
+
+    private void displayMenu(){
+        System.out.println("""
+                --- MENU RENDEZ-VOUS ---
+                1 - Afficher un RDV
+                2 - Afficher tous les RDV
+                3 - Ajouter un RDV
+                4 - Supprimer un RDV
+                5 - Modifier un RDV
+                6 - Quitter
+                """);
+    }
+
+    private void mapChoix(int choix){
+        switch (choix){
+            case 1 -> displayOne();
+            case 2 -> displayAll();
+            case 3 -> insert();
+            case 4 -> delete();
+            case 5 -> update();
+            case 6 -> quit();
+            default -> System.out.println("Choix invalide");
+        }
+    }
+
+    private void displayAll(){
+        service.getAll()
+                .forEach( System.out::println );
+    }
+    private void displayOne(){
+
+        System.out.println("ID du rendez-vous");
+        Long id = Long.parseLong(sc.nextLine());
+
+        try {
+            System.out.println( service.getOne(id) );
+        } catch (ElementNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    private void insert(){
+        System.out.println("Un faux element va être inséré");
+        try {
+            service.insert(new AppointmentDTO());
+        } catch (ElementAlreadyPresentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void delete(){
+        System.out.println("ID du RDV : ");
+        Long id = Long.parseLong(sc.nextLine());
+
+        try {
+            service.delete(id);
+            System.out.println("Le rendez-vous associé à l'ID" + id + " a été supprimé avec succès.");
+        } catch (ElementNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void update(){
+        System.out.println("à implémenter");
+    }
+
+    private void quit(){
+        System.out.println("Au revoir!");
+    }
 }
